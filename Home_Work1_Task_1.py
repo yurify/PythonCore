7e9793b0349aca225df3c5adbcc8477c1ecb8d61
George P <ypcomm@yahoo.com>
2019-11-08 11:57:29 +0200

    HW done

---
+++ Home_Work1_Task_1.py
@@ -0,0 +1,16 @@
+#Home_Work1_Task_1
+
+#Define variables a and b. Read values a and b from console and calculate: 
+#a + b 
+#a - b 
+#a * b 
+#a / b. 
+#Output obtained results.
+
+a = int(input("Please enter a:"))
+b = int(input("Please enter b:"))
+
+print("a + b = ", a + b)
+print("a - b = ", a - b)
+print("a / b = ", a / b)
+print("a * b = ", a * b)
---
+++ Home_Work1_Task_2.py
@@ -0,0 +1,10 @@
+# Home_Work1_Task_2
+# Output question “What is your name?“, “How old are you?“, Where do you live?“. 
+# Read the answer of user and output next information: “Hello
+# , (answer(name))“, “Your age is  (answer(age))“, “You live in  (answer(city))“
+
+name = input("What is your name?\n")
+age = int(input("How old are you?\n"))
+address = input("Where do you live?\n")
+
+print("Hello,", name, "Your age is", age, "You live in", address)
