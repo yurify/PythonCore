36fec47167bba76a4cf4dd2280f537daacd2d771
Yurii Parfeniuk <ypcomm@yahoo.com>
2019-11-25 00:44:07 +0200

    HW Done

---
+++ Double_char.py
@@ -0,0 +1,8 @@
+def double_char(s):
+    new_st = ''
+    for i in s:
+        new_st += i*2
+    return new_st
+
+
+print(double_char("hello"))
---
+++ Grasshopper_Summation.py
@@ -0,0 +1,5 @@
+def summation(num):
+    sum = 0
+    for each_num in range(1, num+1):
+        sum = sum + each_num
+    return sum
