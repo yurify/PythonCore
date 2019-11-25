59e33ebff581d0a454af1c992494f4381ef670b5
yurify <37085772+yurify@users.noreply.github.com>
2019-11-25 14:49:38 +0200

    Create Multiples_of_3_or_5.py
    HM done

---
+++ Multiples_of_3_or_5.py
@@ -0,0 +1,9 @@
+def solution(number):
+    sum = 0
+    for i in range(number):
+        if i % 3 == 0 or i % 5 == 0:
+            if i % 3 == 0 and i % 5 == 0:
+                sum += i
+            else:
+                sum += i
+    return sum
